d0d9c032dbfd426cfbd7d5b32203afda0ef001e8
wookkii <wookkii@home>
2020-09-07 07:09:14 +1000

    Change name to something more generic

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'everydayrewards-ds',
+  namespace: 'stencil-storybook-ds',
   outputTargets: [
     {
       type: 'dist',
